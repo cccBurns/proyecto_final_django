c17caae65e7b3120cb48c0c84095dca57ecf1231
christian burns <christianburns03@gmail.com>
2023-11-14 09:22:23 -0300

    martes14 9:22

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # proyecto_final_django
+
+Este proyecto es desarrollado en Python utilizando el framework Django. El proyecto trata de una app web sobre Hardware, la cual renderiza la informacion que esta almacenadas en la base de datos y la muesta en las diferentes vistas dependiendo cual sea la solicitud.
+
+Models.py:
+
+Forms.py:
+
+Views.py:
+
+Urls.py: Contiene cada una de las rutas de las vistas de la app.
+
+Templates: Es una carpeta donde se encuentran todos los archivos HTML,usados por la app. Se utilza una platilla de BOOSTRAP y se aplica el concep de herencia a cada archivo.
+
+Autor: Christian burns
--- inicio/forms.py
+++ inicio/forms.py
@@ -1,14 +1,26 @@
 from django import forms
-    
-class CrearMonitorFormulario(forms.Form):
-    tipo = forms.CharField(max_length=50)
+from inicio.models import Monitor
+#from ckeditor.fields import RichTextFormField
+
+class BaseMonitorFormulario(forms.Form):
     marca = forms.CharField(max_length=30)
-    modelo = forms.CharField(max_length=40)
     descripcion = forms.CharField(max_length=250)
     anio = forms.IntegerField()
+    
+class CrearMonitorFormulario(BaseMonitorFormulario):
+    tipo = forms.CharField(max_length=50)
+    modelo = forms.CharField(max_length=40)
+    
     
 class BusquedaMonitorFormulario(forms.Form):
     tipo = forms.CharField(max_length=50, required=False)
     marca = forms.CharField(max_length=30, required=False)
     modelo = forms.CharField(max_length=40, required=False)
+    
+class CrearMonitorFormulario(BaseMonitorFormulario):
+    ...
+
+
+class ActualizarMonitorFormulario(BaseMonitorFormulario):
+    ...
     
